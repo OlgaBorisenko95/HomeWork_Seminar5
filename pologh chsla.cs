9207d59627e04773768df3732053f21409779ac7
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-22 13:46:20 +0300

    Нашли в среди рандомных чисел положительные

---
+++ pologh chsla.cs
@@ -0,0 +1,14 @@
+﻿int[]array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+}
+Console.WriteLine($"[{string.Join(",",array)}]");
+
+int count = 0;
+for(int i=0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {count++;};
+}
+Console.WriteLine($"-> {count} ");
